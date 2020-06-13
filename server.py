09398a9056e180c183ce4a00d39dc1b5b0b4e998
joller134 <jasper-zw@hotmail.nl>
2020-06-13 20:42:03 +0200

    added location

--- server.py
+++ server.py
@@ -14,13 +14,16 @@ app = Flask(__name__)
 def index():
     stat = 1
     while stat==1:
-        postId = randint(1,100)
+        postId = randint(1,20)
         caller = 'Post '+str(postId)
         if(data[caller]['URL'][-4:]=='.jpg'):
             title = data[caller]['Title']
             url = data[caller]['URL']
+            lat = data[caller]['lat']
+            lon = data[caller]['lon']
             stat = 0
-    return render_template('index.html', url=url, title=title)
+            print("current data: ",data[caller])
+    return render_template('index.html', url=url, title=title, lat=lat, lon=lon)
 
 if __name__ == '__main__':
-    app.run(port="80",debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
