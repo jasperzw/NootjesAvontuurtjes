d8286a2ccc879a206bbe98fab142253c9df0759a
joller134 <jasper-zw@hotmail.nl>
2020-06-16 09:45:46 +0200

    fix

--- server.py
+++ server.py
@@ -11,18 +11,16 @@ with open('totalAsiaGeolocatedSorted.json') as json_file:
 app = Flask(__name__)
 @app.route('/')
 def index():
-    stat = 1
-    while stat==1:
         postId = randint(1,len(data))
         caller = 'Post '+str(postId)
-        if(data[caller]['URL'][-4:]=='.jpg'):
-            title = data[caller]['Title']
-            url = data[caller]['URL']
-            lat = data[caller]['lat']
-            lon = data[caller]['lon']
-            stat = 0
-            print("current data: ",data[caller])
-    return render_template('index.html', url=url, title=title, lat=lat, lon=lon)
+        print("length data: ",len(data))
+        print("printing at post: ",postId)
+        title = data[caller]['Title']
+        url = data[caller]['URL']
+        lat = data[caller]['lat']
+        lon = data[caller]['lon']
+        stat = 0
+        return render_template('index.html', url=url, title=title, lat=lat, lon=lon)
 
 if __name__ == '__main__':
     app.run(port="80",debug=True, host='0.0.0.0')
